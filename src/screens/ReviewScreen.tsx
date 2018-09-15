3f3c2f1c4946bca1cc979547f6b10d9568707668
a.masalov <delsol@bk.ru>
2018-09-16 01:57:01 +0300

    add elements

--- src/screens/ReviewScreen.tsx
+++ src/screens/ReviewScreen.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { NavigationScreenProps } from 'react-navigation';
 
 interface Props {}
 export default class ReviewScreen extends Component<Props> {
-  static navigationOptions = {
+  static navigationOptions = ({ navigation }: NavigationScreenProps) => ({
     title: 'Review Jobs',
-    headerRight: <Text>back</Text>,
-  };
+    headerRight: <Button title="Settings" onPress={() => navigation.navigate('SettingsScreen')} />,
+  });
 
   render() {
     return (
